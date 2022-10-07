b2d2d1754da4d5c0db87f550c198a1e36fe47a2b
Artur Shiabutdinov <hypnorus@yandex.ru>
2022-10-08 01:15:05 +0300

    sprint 6 iteration 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,181 +1,121 @@
-let profilePopup = document.querySelector('.profile-popup');
-let editButton = document.querySelector('.profile__edit-info');
-let profileClosePopup = document.querySelector('.profile-popup__close');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let profilePopupName = document.querySelector('.profile-popup__input_type_name');
-let profilePopupJob = document.querySelector('.profile-popup__input_type_job');
-let profileForm = document.querySelector('.profile-popup__content');
+const profilePopup = document.querySelector('.profile-popup');
+const buttonEditProfile = document.querySelector('.profile__edit-info');
+const profileClosePopup = document.querySelector('.profile-popup__close');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const profilePopupName = document.querySelector('.profile-popup__input_type_name');
+const profilePopupJob = document.querySelector('.profile-popup__input_type_job');
+const profileForm = document.querySelector('.profile-popup__content');
+const cardImageView = document.querySelector('.view-mesto-popup__image');
+const cardNameView = document.querySelector('.view-mesto-popup__name');
+const template = document.querySelector('.template');
+const cards = document.querySelector('.elements');
+const cardAddPopup = document.querySelector('.add-mesto-popup');
+const cardAddButton = document.querySelector('.profile__add-element');
+const cardAddClosePopup = document.querySelector('.add-mesto-popup__close');
+const cardAddForm = document.querySelector('.add-mesto-popup__content');
+const cardAddName = document.querySelector('.add-mesto-popup__input_type_mesto');
+const cardAddLink = document.querySelector('.add-mesto-popup__input_type_image');
+const cardViewPopup = document.querySelector('.view-mesto-popup');
+const cardViewClosePopup = document.querySelector('.view-mesto-popup__close');
 
-editButton.addEventListener('click', profilePopupOpening);
-profileClosePopup.addEventListener('click', profilePopupClosing);
-profileForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  profileJob.textContent = profilePopupJob.value;
-  profileName.textContent = profilePopupName.value;
-  profilePopupClosing();
-});
-
-function profilePopupOpening() {
-  profilePopup.classList.add('profile-popup_opened');
-  profileInfoFilling();
+// Открываем попапы
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function profilePopupClosing() {
-  profilePopup.classList.remove('profile-popup_opened');
+// Закрываем попапы
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-function profileInfoFilling() {
+// Создаем карточку
+function createCard(cardName, cardImage) {
+  // Получаем элементы
+  const card = template.content.cloneNode(true);
+  const cardImageDom = card.querySelector('.element__image');
+  const cardNameDom = card.querySelector('.element__name');
+  const cardLikeButton = card.querySelector('.element__like');
+  const cardDeleteButton = card.querySelector('.element__remove');
+
+  // Даём им присланные значения
+  cardImageDom.setAttribute('src', cardImage);
+  cardImageDom.setAttribute('alt', cardName);
+  cardNameDom.textContent = cardName;
+
+  // Вешаем слушатели
+  cardDeleteButton.addEventListener('click', () => {
+    cardDeleteButton.closest('.element').remove();
+  });
+
+  cardLikeButton.addEventListener('click', () => {
+    cardLikeButton.classList.toggle('element__like_active');
+  });
+
+  cardImageDom.addEventListener('click', () => {
+    openPopup(cardViewPopup);
+    cardImageView.setAttribute('src', cardImageDom.getAttribute('src'));
+    cardImageView.setAttribute('alt', cardImageDom.getAttribute('alt'));
+    cardNameView.textContent = cardImageDom.getAttribute('alt');
+  });
+
+  return card;
+}
+
+// Рендерим карточку
+function renderCard(card) {
+  cards.prepend(card);
+}
+
+// Заполняем форму профиля
+function fillProfileInfoForm() {
   profilePopupName.value = profileName.textContent;
   profilePopupJob.value = profileJob.textContent;
 }
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+// Рендерим начальные карточки
+initialCards.forEach((item => {
+  renderCard(createCard(item.name, item.link))
+}));
 
-initialCards.forEach((item) => {
-  const cardName = item.name;
-  const cardSrc = item.link;
-
-  const elements = document.querySelector('.elements');
-  const template = document.querySelector('.template');
-  const element = template.content.cloneNode(true);
-
-  const elementImage = element.querySelector('.element__image');
-  elementImage.setAttribute('src', cardSrc);
-  elementImage.setAttribute('alt', cardName);
-
-  const elementDetails = element.querySelector('.element__details');
-  const elementName = elementDetails.querySelector('.element__name');
-  elementName.textContent = cardName;
-
-  elements.append(element);
+// Открываем попап профиля и предзаполняем форму
+buttonEditProfile.addEventListener('click', () => {
+  openPopup(profilePopup);
+  fillProfileInfoForm();
 });
 
-const addMestoPopup = document.querySelector('.add-mesto-popup');
+// Закрываем попап профиля
+profileClosePopup.addEventListener('click', () => {
+  closePopup(profilePopup);
+});
 
-const addMestoButton = document.querySelector('.profile__add-element');
-
-const addMestoClosePopup = document.querySelector('.add-mesto-popup__close');
-
-const addMestoForm = document.querySelector('.add-mesto-popup__content');
-
-const addMestoName = document.querySelector('.add-mesto-popup__input_type_mesto');
-const addMestoLink = document.querySelector('.add-mesto-popup__input_type_image');
-
-addMestoButton.addEventListener('click', addMestoPopupOpening);
-addMestoClosePopup.addEventListener('click', addMestoPopupClosing);
-addMestoForm.addEventListener('submit', function (e) {
+// Сабмитим форму профиля и закрываем попап
+profileForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  const elements = document.querySelector('.elements');
-  const template = document.querySelector('.template');
-  const element = template.content.cloneNode(true);
-
-  const elementImage = element.querySelector('.element__image');
-  elementImage.setAttribute('src', addMestoLink.value);
-  elementImage.setAttribute('alt', addMestoName.value);
-
-  const elementDetails = element.querySelector('.element__details');
-  const elementName = elementDetails.querySelector('.element__name');
-  elementName.textContent = addMestoName.value;
-
-  const elementLike = element.querySelector('.element__like');
-  elementLike.addEventListener('click', () => {
-    elementLike.classList.toggle('element__like_active');
-  });
-
-  const elementRemove = element.querySelector('.element__remove');
-  elementRemove.addEventListener('click', () => {
-    elementRemove.closest('.element').remove();
-  });
-
-  const mestoImage = document.querySelector('.view-mesto-popup__image');
-  const mestoName = document.querySelector('.view-mesto-popup__name');
-
-  elementImage.addEventListener('click', () => {
-    viewMestoPopupOpening();
-    mestoImage.setAttribute('src', elementImage.getAttribute('src'));
-    mestoImage.setAttribute('alt', elementImage.getAttribute('alt'));
-    mestoName.textContent = elementImage.getAttribute('alt');
-  });
-
-  elements.prepend(element);
-
-  addMestoPopupClosing();
+  profileJob.textContent = profilePopupJob.value;
+  profileName.textContent = profilePopupName.value;
+  closePopup(profilePopup);
 });
 
-function addMestoPopupOpening() {
-  addMestoPopup.classList.add('add-mesto-popup_opened');
-  addMestoName.value = '';
-  addMestoLink.value = '';
-}
-
-function addMestoPopupClosing() {
-  addMestoPopup.classList.remove('add-mesto-popup_opened');
-}
-
-const elementLikes = document.querySelectorAll('.element__like');
-
-elementLikes.forEach((item) => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('element__like_active');
-  })
+// Открываем попап создания новой карточки
+cardAddButton.addEventListener('click', () => {
+  openPopup(cardAddPopup);
 });
 
-const elementRemoves = document.querySelectorAll('.element__remove');
-
-elementRemoves.forEach((item) => {
-  item.addEventListener('click', () => {
-    item.closest('.element').remove();
-  })
+// Закрываем попап создания новой карточки
+cardAddClosePopup.addEventListener('click', () => {
+  closePopup(cardAddPopup);
 });
 
-const viewMestoPopup = document.querySelector('.view-mesto-popup');
+// Сабмитим форму создания новой карточки и закрываем попап
+cardAddForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  renderCard(createCard(cardAddName.value, cardAddLink.value))
+  cardAddForm.reset();
+  closePopup(cardAddPopup);
+});
 
-function viewMestoPopupOpening() {
-  viewMestoPopup.classList.add('view-mesto-popup_opened');
-}
-
-function viewMestoPopupClosing() {
-  viewMestoPopup.classList.remove('view-mesto-popup_opened');
-}
-
-const viewMestoClosePopup = document.querySelector('.view-mesto-popup__close');
-viewMestoClosePopup.addEventListener('click', viewMestoPopupClosing);
-
-const mestoImages = document.querySelectorAll('.element__image');
-const mestoImage = document.querySelector('.view-mesto-popup__image');
-const mestoName = document.querySelector('.view-mesto-popup__name');
-
-mestoImages.forEach((item) => {
-  item.addEventListener('click', () => {
-    viewMestoPopupOpening();
-    mestoImage.setAttribute('src', item.getAttribute('src'));
-    mestoImage.setAttribute('alt', item.getAttribute('alt'));
-    mestoName.textContent = item.getAttribute('alt');
-  })
+// Закрываем попап просмотра карточки
+cardViewClosePopup.addEventListener('click', () => {
+  closePopup(cardViewPopup);
 });
